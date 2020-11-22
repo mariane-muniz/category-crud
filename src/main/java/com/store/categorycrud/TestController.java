f6630fd2b8dc48acce326e81a996722c1a544b02
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2020-11-22 13:40:25 -0300

    update

--- src/main/java/com/store/categorycrud/TestController.java
+++ src/main/java/com/store/categorycrud/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/test")
 public class TestController {
     
     @GetMapping
