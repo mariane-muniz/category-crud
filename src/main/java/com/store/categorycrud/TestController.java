e4c9529e5a3a7bc1bf55aac37e232c20280f951d
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2020-11-22 10:26:03 -0300

    first commit

---
+++ src/main/java/com/store/categorycrud/TestController.java
@@ -0,0 +1,16 @@
+package com.store.categorycrud;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+    
+    @GetMapping
+    public String test(){
+        return "Works";
+    }
+    
+}
